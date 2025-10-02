51b8e51a017799dacb6c1e9d9f8afc2737df8602
Nivedhan-N <nivedhannive9@.com>
2025-10-02 14:22:22 +0530

    added t1.java file

---
+++ t1.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+class t1 {
+    public static void main(String args[])
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("what is your Scaler");
+        int scalar = scan.nextInt();
+        System.out.println("what is ypur Age");
+        int age = scan.nextInt();
+     
+    if(scalar<=20000||age<=25){
+        System.out.println("Eligible for loan");
+        System.out.println("how much loan do you need");
+        int loan = scan.nextInt();
+    
+        if(loan<=50000){
+            System.out.print("loan avalable");
+    }
+    else if (loan>50000) {
+        System.out.println("Maximum loan amount is 50000");
+        
+    } 
+}
+    else {
+        System.out.print("You are not eligible for loan");
+    }
+}
+    }
